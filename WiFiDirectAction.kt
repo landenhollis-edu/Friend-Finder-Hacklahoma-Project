bc1921c8011e2796fd0027f0c74859c469a05ffa
SamASpr <143137190+SamASpr@users.noreply.github.com>
2025-02-15 21:49:45 -0600

    Add files via upload

--- WiFiDirectAction.kt
+++ WiFiDirectAction.kt
@@ -8,7 +8,12 @@ import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
+import android.net.wifi.WpsInfo
+import android.net.wifi.p2p.WifiP2pConfig
 import android.net.wifi.p2p.WifiP2pDevice
+import android.util.Log
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 
 
@@ -40,6 +45,8 @@ class WiFiDirectAction : AppCompatActivity(){
         unregisterReceiver(receiver)
     }
 
+
+
 }
 
 /**
@@ -49,27 +56,96 @@ public class WiFiDirectBroadcastReceiver (
     private val manager: WifiP2pManager,
     private val channel: WifiP2pManager.Channel,
     private val activity: Activity
-) : BroadcastReceiver() {
 
+) : BroadcastReceiver() {
+    private val peers = mutableListOf<WifiP2pDevice>()
     override fun onReceive(context: Context, intent: Intent) {
         val action: String = intent.action.toString()
+        var isWifiP2pEnabled = false
+
+        val peerListListener = WifiP2pManager.PeerListListener { peerList ->
+            val refreshedPeers = peerList.deviceList
+            if (refreshedPeers != peers) {
+                peers.clear()
+                peers.addAll(refreshedPeers)
+
+                // If an AdapterView is backed by this data, notify it
+                // of the change. For instance, if you have a ListView of
+                // available peers, trigger an update.
+
+
+                // Perform any other updates needed based on the new list of
+                // peers connected to the Wi-Fi P2P network.
+            }
+
+            if (peers.isEmpty()) {
+                Log.d(TAG, "No devices found")
+                return@PeerListListener
+            }
+        }
         when (action) {
             WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
-                // Check to see if Wi-Fi is enabled and notify appropriate activity
+                // Determine if Wi-Fi Direct mode is enabled or not, alert
+                // the Activity.
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                when (state) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        isWifiP2pEnabled = true
+                    }
+
+                    else -> {
+                        isWifiP2pEnabled = false
+                    }
+                }
             }
+
             WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
+                manager.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+
+                    override fun onSuccess() {
+                        manager.requestPeers(channel, peerListListener)
+                        Log.d(TAG, "P2P peers changed")
+                    }
+
+                    override fun onFailure(reasonCode: Int) {
+                        //try again
+                    }
+                })
             }
+
             WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
                 // Respond to new connection or disconnections
             }
+
             WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
-                // Respond to this device's wifi state changing
+                // Respond to changes in this device's router
             }
         }
+
     }
+    fun connect() {
+        // Picking the first device found on the network.
+        val device = peers[0]
 
+        val config = WifiP2pConfig().apply {
+            deviceAddress = device.deviceAddress
+            wps.setup = WpsInfo.PBC
+        }
 
+        manager.connect(channel, config, object : WifiP2pManager.ActionListener {
+
+            override fun onSuccess() {
+                // WiFiDirectBroadcastReceiver notifies us. Ignore for now.
+            }
+
+            override fun onFailure(reason: Int) {
+                connect()
+            }
+        })
+    }
 }
+
+
 
 
